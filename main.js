36a43c3903fbc973b9002ccecd67a9840ba04de2
Eduardo Pereira <du@costapereira.com>
2023-09-07 12:03:39 -0300

    primeiro commit intensivao javascript hastag

---
+++ main.js
@@ -0,0 +1,13 @@
+import { renderizarCatalogo } from "./src/cartaoProduto";
+import { inicializarFiltros } from "./src/filtrosCatalogo";
+import { inicializarCarrinho, renderizarProdutosCarrinho, atualizarPrecoCarrinho } from "./src/menuCarrinho";
+
+
+  renderizarCatalogo();
+  inicializarCarrinho();
+  renderizarProdutosCarrinho();
+  atualizarPrecoCarrinho();
+  inicializarFiltros();
+
+
+// alert("opa, olha o javascript aqui funcionando!")
